377ad2dc7a272e4481eb68f43ec89d16e2eb875e
c3to <c3to@web.de>
2018-03-11 11:48:52 +0100

    init

---
+++ src/scripts.js
@@ -0,0 +1,3 @@
+const css = require('./styles.less');
+
+console.log('###  works  ###');
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLESS = new ExtractTextPlugin('styles.css');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: {
+    scripts: "./src/scripts.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+    filename: "[name].min.js",
+  },
+  module: {
+    rules: [{
+      test: /\.less$/,
+      use: extractLESS.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader'],
+          publicPath: '/dist',
+        })
+    },{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+    }]
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, "dist"),
+    stats: "errors-only",
+  },
+  plugins: debug ? [
+    new HtmlWebpackPlugin({
+      title: 'ES6 Starterkit',
+      // minify: {
+        //   collapseWhitespace: true
+        // },
+      filename: './../index.html',
+      // hash: true,
+      template: './src/index.html',
+    }),
+    extractLESS,
+  ] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
