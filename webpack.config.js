438104797c152cd5b41db74a9fd3a0a089eb91b8
toabr <25079664+toabr@users.noreply.github.com>
2021-08-23 15:39:43 +0200

    versionbump

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "dist",
+    // publicPath: "dist",
     filename: "[name].min.js",
   },
   module: {
@@ -21,27 +21,35 @@ module.exports = {
         loader: "babel-loader"
       }
     },{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'postcss-loader', 'sass-loader'],
-        // publicPath: '/dist',
-      })
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        false ? "style-loader" : MiniCssExtractPlugin.loader,
+        "css-loader",
+        "postcss-loader",
+        "sass-loader",
+      ],
     }]
   },
   devServer: {
-    stats: "errors-only",
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    // stats: "errors-only",
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Starterkit es6',
       hash: true,
       template: './src/index.html',
-      filename: './../index.html',
+      filename: 'index.html',
       minify: {
         collapseWhitespace: true
       },
     }),
-    new ExtractTextPlugin('style.css')
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    })
   ]
 };
